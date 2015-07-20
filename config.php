f8f3bbbb522fa332385c6511f773be4fee7444c8
nadiadev <creation.nadia@gmail.com>
2015-07-20 14:43:17 +0200

    initial : prepare the define

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+    define("DBHOST", "localhost"); //ip du serveur MySQL
+    define("DBUSER", "root"); 		//username MySQL
+    define("DBPASS", "");			//mot de passe MySQL
+    define("DBNAME", "auth"); 	//nom de la bdd
