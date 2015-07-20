f0615324abfdf33a0614ea5f8f059f38d9567a7d
nadiadev <creation.nadia@gmail.com>
2015-07-20 14:44:10 +0200

    initial : prepare db

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+	try {
+		//crée un objet PDO
+		$dbh = new PDO(
+			'mysql:host='.DBHOST.';dbname='.DBNAME, 	//dsn
+			DBUSER, 	//username 
+			DBPASS, //mot de passe
+			array(
+				PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", //pour communiquer en utf8 avec le serveur MySQL
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //on veut recevoir des arrays associatifs, dans les requêtes SELECT
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING //on veut afficher toutes les erreurs MySQL
+			)
+		);
+	} catch (PDOException $e) {
+	    echo 'Erreur de connexion : ' . $e->getMessage();
+	}
