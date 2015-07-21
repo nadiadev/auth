463877fd35083263bd848053fc07dcfe925cb477
nadiadev <creation.nadia@gmail.com>
2015-07-21 10:15:27 +0200

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    define("DBHOST", "localhost"); //ip du serveur MySQL
-    define("DBUSER", "root"); 		//username MySQL
-    define("DBPASS", "");			//mot de passe MySQL
-    define("DBNAME", "auth"); 	//nom de la bdd
