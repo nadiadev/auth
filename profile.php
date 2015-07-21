aebef1fdd2362d3c9342ca8db8b8137cd3d7ac77
nadiadev <creation.nadia@gmail.com>
2015-07-21 15:04:06 +0200

    lock profile page to connected users

--- profile.php
+++ profile.php
@@ -1,9 +1,18 @@
 <?php
 	session_start();
 
+	include("functions.php");
+
+	pr($_SESSION);
+
+
 	//vérification que l'utilisateur est bien connecté
 
 	//si l'utilisateur n'est pas connecté, on le redirige vers login.php
+	if (empty($_SESSION['user'])){
+		header("Location: login.php");
+		die();
+	}
 
 	//sinon... on ne fait rien et la page ci-dessous s'affichera
 ?>
@@ -15,7 +24,10 @@
 	<title>Profile</title>
 </head>
 <body>
-	<h1>Profil de <?php //pseudo de l'user ?></h1>
+
+	<a href="logout.php" title="Me déconnecter de mon compte">Déconnexion</a>
+	<h1>Profil de <?php echo $_SESSION['user']['username'];//pseudo de l'user ?></h1>
+	<p>Cette page ne devrait être accessible que pour les utilisateurs conncectés.</p>
 	
 </body>
 </html>
