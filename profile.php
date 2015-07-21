ce904e52103ba84792af9ad4d39288567ebaa10c
nadiadev <creation.nadia@gmail.com>
2015-07-21 13:00:49 +0200

    add base for profil page

---
+++ profile.php
@@ -0,0 +1,21 @@
+<?php
+	session_start();
+
+	//vérification que l'utilisateur est bien connecté
+
+	//si l'utilisateur n'est pas connecté, on le redirige vers login.php
+
+	//sinon... on ne fait rien et la page ci-dessous s'affichera
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Profile</title>
+</head>
+<body>
+	<h1>Profil de <?php //pseudo de l'user ?></h1>
+	
+</body>
+</html>
